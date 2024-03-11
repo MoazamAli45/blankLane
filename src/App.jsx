e884802322f9446529813493cd00cd63023ff4ff
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-03-11 22:59:35 +0500

    Refactor routing in App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CityToCity from "./pages/CityToCity";
 import { Home } from "./components/Home/Home";
@@ -10,16 +10,14 @@ import LimousineServices from "./components/LimousineServices/LimousineServices"
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/city-to-city" element={<CityToCity />}></Route>
-          <Route path="/airportTransfer" element={<AirportTransfer />}></Route>
-          <Route path="/hourlyHire" element={<HourlyHire />} />
-          <Route path="/choufferservices" element={<ChoufferServices />} />
-          <Route path="/limousineservices" element={<LimousineServices />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city-to-city" element={<CityToCity />} />
+        <Route path="/airportTransfer" element={<AirportTransfer />} />
+        <Route path="/hourlyHire" element={<HourlyHire />} />
+        <Route path="/choufferservices" element={<ChoufferServices />} />
+        <Route path="/limousineservices" element={<LimousineServices />} />
+      </Routes>
     </>
   );
 }
